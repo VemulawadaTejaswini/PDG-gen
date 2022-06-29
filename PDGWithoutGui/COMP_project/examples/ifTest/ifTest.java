f2c625e7c5b4764c6bab003bb7f7e079d5f91a3c
nipuna <nipuna@nipunas-MacBook-Pro.local>
2022-06-29 21:48:20 +0530

    ParsingFileFixed

--- PDGWithoutGui/COMP_project/examples/ifTest/ifTest.java
+++ PDGWithoutGui/COMP_project/examples/ifTest/ifTest.java
@@ -5,11 +5,15 @@ public class test3{
         while (i < 11) {
             sum = sum + i;
             i = i + 1;
-            i++;
+            do {
+				j=j+1;
+			} while(j < 5);
         }
         if(i > 0) {
+            if(sum>0){
             System.out.println(sum);
             System.out.println(i);
         }
+    }
     }
 }
