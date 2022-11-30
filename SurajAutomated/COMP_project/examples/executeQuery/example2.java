ecbfffc7231f93a3bde4c825e9eeaa428e6d0da6
= <=>
2022-11-30 16:45:53 +0530

    More PDG graph examples added

---
+++ SurajAutomated/COMP_project/examples/executeQuery/example2.java
@@ -0,0 +1,12 @@
+//1
+public class func{
+	public void testArray(Connection conn){
+        prep.setObject(1, new Object[] { new BigDecimal("1"), "2" });
+        ResultSet rs = prep.executeQuery();
+        rs.next();
+        assertEquals("1", rs.getString(1));
+        rs.next();
+        assertEquals("2", rs.getString(1));
+        assertFalse(rs.next());
+}
+}
